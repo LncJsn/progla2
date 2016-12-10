07d891e1b4b9be907323ddea8f6ef533aa49da0a
Lance Jeison V. Doon <lancejeison28@gmail.com>
2016-12-10 18:40:18 +0800

    Create edpogi.cpp

---
+++ edpogi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	
+	for (int x=1; x<=100; x++ )
+	 	if (x % 3 == 0)
+		{
+			cout << "Pogi \n" ;
+		}
+		
+	 	 else if (x % 5 == 0)
+		{
+			cout << "Ed \n" ;
+		}
+		
+	 	 else if (x % 3 == 0 && x % 5 == 0 )
+		{
+			cout << "Ed Pogi \n" ;
+		}
+		else 
+		{
+			cout << x << endl;
+		}
+		
+
+	return 0;
+}
+
