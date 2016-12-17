c2198625b6b65d0b7aabbd8de6ea4da73dfce8c3
LncJsn <lancejeison28@gmail.com>
2016-12-17 16:45:08 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -9,4 +9,25 @@ struct Person
 	int shoulder;
 	int head;
 };
-
+ int main()
+ {
+	 cout << "Person's toes:" << endl;
+	 cin >> Person.toes;
+	 cout << "Person's knees:" << endl;
+	 cin >> Person.knees;
+	 cout << "Person's shoulder:" << endl;
+	 cin >> Person.shoulder;
+	 cout << "Person's head:" << endl;
+	 cin >> Person.head;
+	 
+	 cout << Person.toes << endl;
+	 cout << Person.knees << endl;
+	 cout << Person.shoulder << endl;
+	 cout << Person.head << endl;
+	 
+	 
+	 
+	 
+	 
+	 return 0;
+ }
