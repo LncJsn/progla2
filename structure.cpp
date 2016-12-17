d018273b0ddfc3e04274b150177840a79a098876
Ivan Ponsoy <mrmvip_16@yahoo.com>
2016-12-17 16:50:45 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -7,6 +7,16 @@ struct Person {
 	int head;	
 };
 int main(){
-
+	Person Person;
+	
+	cin>>Person.toes;
+	cin>>Person.knees;
+	cin>>Person.shoulder;
+	cin>>Person.head;
+	
+	cout<<Person.toes;
+	cout<<Person.knees;
+	cout<<Person.shoulder;
+	cout<<Person.head;
 	return 0;
 }
